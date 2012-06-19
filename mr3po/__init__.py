db38146c7266d456607e9c50d0cc4f6c23c8cf63
David Marin <dave@yelp.com>
2012-06-19 12:04:57 -0700

    bump version number

--- mr3po/__init__.py
+++ mr3po/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Yelp
+# Copyright 2011-2012 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 __author__ = 'David Marin <dave@yelp.com>'
 
 __credits__ = [
+    # You?
 ]
 
-__version__ = '0.1.0-dev'
+__version__ = '0.1.0'
